009e9249dc55f322eef6b2b3805d9f2faea39099
Sergii Serdiuk <serdiuk-sergii@bustour.ua>
2024-10-29 20:22:18 +0200

    feat: add components HeroGraph

--- src/types/types.ts
+++ src/types/types.ts
@@ -36,3 +36,41 @@ export interface PaginateProps {
   pageCount: number;
   handlePageClick: (event: { selected: number }) => void;
 }
+
+export interface HeroGraphProps {
+  heroName: string;
+  birth_year: string;
+  eye_color: string;
+  gender: string;
+  hair_color: string;
+  height: string;
+  mass: string;
+  skin_color: string;
+  homeworld: string;
+  films: string[];
+  starships: string[];
+  heroImage: string;
+  filmNames: string[];
+  close: () => void;
+}
+
+export interface FilmNodeProps {
+  filmName: string;
+}
+
+export interface HeroNodeProps {
+  heroImage: string;
+  heroName: string;
+  birth_year: string;
+  eye_color: string;
+  gender: string;
+  hair_color: string;
+  height: string;
+  mass: string;
+  skin_color: string;
+  homeworld: string;
+}
+
+export interface StarshipNodeProps {
+  name: string;
+}
